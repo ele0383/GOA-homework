20a18f88df35915e28329c6d48ba756f355ccdf1
ele0383 <chemiaele000@gmail.com>
2024-09-26 11:02:09 -0700

    comit

---
+++ Day 03/homework/lasson3.py
@@ -0,0 +1,45 @@
+# num1 = 11
+# num2 = 8
+# print(num1 + num2)
+
+# age = 11
+# haight = 155
+# weight = 36
+# print(age, haight, weight)
+
+# film1 = "padminktoni"
+# film2 = "tom and jarri"
+# print(film1, film2,)
+
+# num1 = 2
+# num2 = 8
+# num3 = 11
+# num4 = 37
+# num5 = 39
+# num6 = 7
+# num7 = 74
+# num8 = 190
+# num9 = 49
+# num10 = 122
+
+
+# num1 = 2.33
+# num2 = 8.99
+# num3 = 7.77
+# num4 = 2.45
+# num5 = 6.8
+# num6 = 9.70
+# num7 = 6.84
+# num8 = 3.12
+# num9 = 9.60
+# num10 = 8.13
+
+
+# num1 = "34"
+# num2 = "19"
+# num3 = "89"
+# num4 = "112"
+# num5 = "6"
+
+# name = "mariami"
+# lastname = "merlani"
