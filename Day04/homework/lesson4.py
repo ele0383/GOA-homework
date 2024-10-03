f49455a9cb3bc52dc1433eb8dc9a36e67ce6f726
ele0383 <chemiaele000@gmail.com>
2024-10-03 12:15:54 -0700

    commit

---
+++ Day04/homework/lesson4.py
@@ -0,0 +1,15 @@
+# num1 = "table"
+# num2 = 20
+# num3 = 23.7
+# num4 = "door"
+# num5 = 55
+
+
+familly1 = "giorgi"
+familly2 = "ekaterine"
+familly3 = "salome"
+familly4 = "mariami"
+print(familly1)
+print(familly2)
+print(familly3)
+print(familly4)
