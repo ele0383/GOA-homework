00751c30460d4106f67979c1c62f0d2b379cf94b
ele0383 <chemiaele000@gmail.com>
2024-11-13 22:22:08 +0400

    commit

--- Day05/hw5.py
+++ Day05/hw5.py
@@ -1,3 +1,53 @@
-name = "elene"
-surname = ""
+# name = "elene"
+# surname = "chemia"
+# print(name,surname)
 
+# film1 = "padminqton"
+# film2 = "tommy and jerri"
+# print(film2)
+
+# num1 = 10
+# num2 = 20
+# num3 = 30
+# print(num2)
+
+# score = 15
+# score = 20
+# score = 25
+# print(score)
+
+# num1 = "foot"
+# num2 = "ball"
+# print(num1+num2)
+
+# name = "salme"
+# name = "mariami"
+# print(name)
+
+# num1 = "GOA"
+# num2 = "BEST"
+# print(num1 + num2)
+
+# score = 30
+# num1 = 30 + 10
+# num2 = 30 - 10
+# num3 = 30 / 10
+# num4 = 30 * 10
+
+# name = "Anna"
+# score = 96
+
+# print (name)
+# print (score)
+
+
+#Storing the flight number
+flight_n = "BA0117" #ფრენის ნომერი-"BAO117"
+
+#Storing the flight information
+destination = "New York" #დანიშნულების ადგილი-"New York"
+distance = 1580 #დიტანცია-1580
+
+print(flight_n)
+print(destination)
+print(distance)
