668231e9c26e26101108683fc5c59853d14b4562
ele0383 <chemiaele000@gmail.com>
2024-11-21 21:52:03 +0400

    commit

---
+++ Day11/classwork/cw11.py
@@ -0,0 +1,5 @@
+print(5==5 and 10<5)
+print(2==2 and 3==3)
+
+print(10<15)
+print(10==10)
---
+++ Day11/homework/hw11.py
@@ -0,0 +1,21 @@
+# 15==10
+# 15>10
+# 15<10
+
+# print(30>35 and 35>30)
+# print(27<30 and 30<27)
+# print(53==53 and 53==87)
+# print(90>=30 and 30>=90)
+# print(33<=25 and 25<=33)
+# print(33!=23 and 33!=33)
+
+
+# print(333>33 or 33>333)
+# print(324<432 or 33<33)
+# print(54==67 or 44==44)
+# print(71>=23 or 29>=29)
+# print(45<=56 or 76<=76)
+# print(732!=222 or 562!=562)
+
+
+# member = input("enter your member")
