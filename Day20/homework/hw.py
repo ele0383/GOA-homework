751051a030b8451c3a35f5eea7906b1c197c6af7
ele0383 <chemiaele000@gmail.com>
2025-01-24 17:48:42 +0400

    commit

---
+++ Day20/homework/hw.py
@@ -0,0 +1,50 @@
+# Number = 7
+# if Number == 7:
+#     print("რიცხვი არის კენტი")
+# else
+#       print("რიცხვი არის ლუწი")  
+
+
+# age = 19
+# if age <18:
+#     print("შენ არ ხარ სრულწლოვანი")
+# else
+#     print("შენ ხარ სრულწლოვწნი")    
+
+
+# Sign = int(input("შეიყვანე შენი ნიშანი"))
+# if Sign > 90:
+#     print("შენ მიიღე A")
+
+# sing = int(input("შეიყვანე შენი ნიშანი"))
+# if sing > 90:
+#     print("შენ მიიღე A")
+# elif sing < 90:
+#     print("შენ მიიღე B")
+# elif sing < 70:
+#     print("შენ მიიღე C")  
+# else:
+#     print("შენ მიიღე D")
+
+
+
+
+
+
+
+    
+
+
+
+   
+
+
+        
+     
+
+ 
+     
+
+
+
+
