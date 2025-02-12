c44a2fd716cdddd4fbed3dcf46ba7d831cb50801
ele0383 <chemiaele000@gmail.com>
2025-02-12 21:40:51 +0400

    commit

---
+++ Day22/classwork/cw.py
@@ -0,0 +1,20 @@
+mark = 8
+if mark <10:
+    print("შენ მიიღე A")
+elif mark > 9:    
+    print("შენ მიიღე A")
+   
+
+if mark < 8:
+    print("შენ მიიღე B")
+elif mark >7
+    print("შენ მიიღე B")
+
+if mark < 6:
+    print("შენ მიიღე C")
+elif mark >5:
+    print("შენ მიიღე C")    
+
+
+
+    
---
+++ Day22/homework/hw.py
@@ -0,0 +1,13 @@
+# correct_password="py"
+# max_attempts = 3
+# for attempts in range(max_attempts):
+#     password = input("შეიყვანეთ პაროლი:")
+
+#     if password == correct_password:
+#         print("correct")
+
+# else:
+#     print("out  of trys")        
+
+
+correct_password = 1,2,3.
