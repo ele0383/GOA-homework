c24e39ee63c0c360c1b29bcc5f6cab6067b589e0
ele0383 <chemiaele000@gmail.com>
2025-04-04 15:31:43 +0400

    commit

---
+++ Day29/classwork/cw.py
@@ -0,0 +1,12 @@
+from turtle import*
+
+begin_fill()
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+end_fill()
+
+exitonclick()
---
+++ Day29/classwork/cw2.py
@@ -0,0 +1,4 @@
+def name(gamarjoba):
+    print("elene")
+name("gamarjoba")   
+         
