99e2cc80200aeb04e60b5ab1e191dbd36ae33410
ele0383 <chemiaele000@gmail.com>
2025-05-09 17:42:17 +0400

    commit

---
+++ Day35/classwork/cw.py
@@ -0,0 +1,24 @@
+# names = ["ელენე","სალომე","თორნიკე","მარიამი"]
+# names.insert(2,"გურამი")
+# print(names)
+
+
+# numbers = ["1","2","3","4"]
+# numbers.append("5")
+# numbers.append("7")
+# numbers.append("9")
+# numbers.append("10")
+
+
+
+# numbers = ["1","2","3","4","5","6"]
+# numbers.pop(3)
+# numbers.pop(4)
+# print(numbers)
+
+
+
+
+# def length(num1,num2,num3):
+#     print(num1+num2+num3)
+# length(5+10+15)    
---
+++ Day35/homework/hw.py
@@ -0,0 +1,10 @@
+# num = ("elene","salome","10.4","112")
+# for i in range(2):
+#     print(num)
+
+# num1 = ["A","B","C","D","E"]
+# num2 = []
+
+# num1 = ["A","B","C","D","E"]
+# num1.append("B","D")
+# print("".upper())
